8a8b0be372286130c0e63c224cbb6366f491c059
Vee <vinitsinghthakur46@gmail.com>
2026-04-14 14:57:53 +0530

    Update llm.py

--- src/llm.py
+++ src/llm.py
@@ -1,1 +1,23 @@
+from transformers import pipeline
 
+# Load once
+generator = pipeline(
+    "text-generation",
+    model="google/flan-t5-base",
+    max_length=512
+)
+
+
+def generate_answer(query, context):
+    prompt = f"""
+    Answer the question based only on the context.
+
+    Context:
+    {context}
+
+    Question:
+    {query}
+    """
+
+    response = generator(prompt)[0]['generated_text']
+    return response
